1f33b0a05a4e5730b62d9aca9a2f1ed89a5361a6
Jonathon Vega <jfvega@live.com>
2025-10-21 13:03:01 -0500

    fixed lint error

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get "collection_items/index"
   get "home/index"
   root "home#index"
-  resources :collection_items, only: [:index, :create, :destroy]
-  resources :wishlist_items, only: [:index, :create, :destroy]
+  resources :collection_items, only: [ :index, :create, :destroy ]
+  resources :wishlist_items, only: [ :index, :create, :destroy ]
   get "search", to: "search#index"
 end
