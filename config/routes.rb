608255b1180cba23af7cf826d2582ca91770cbf5
Amira8399 <amiraw_2007@tamu.edu>
2025-10-22 21:19:11 -0500

    Task #8 – Finalize login screen

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
-  get "search/index"
+  get 'login',  to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
   get "wishlist_items/index"
   get "collection_items/index"
   get "home/index"
   root "home#index"
-  resources :collection_items, only: [ :index, :create, :destroy ]
-  resources :wishlist_items, only: [ :index, :create, :destroy ]
+
+  resources :collection_items, only: [:index, :create, :destroy]
+  resources :wishlist_items, only: [:index, :create, :destroy]
   get "search", to: "search#index"
 end
