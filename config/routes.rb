8bb2f029567f5680e8d0e5bea5355e30bd7c9d56
Amira8399 <amiraw_2007@tamu.edu>
2025-10-27 19:08:52 -0500

    Add My Collection index page

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   get "search/index"
   get "wishlist_items/index"
   get "collection_items/index"
-  get "home/index"
-  root "home#index"
+  get "collection", to: "albums#collection", as: :collection
+  
+  root "albums#index"
   resources :collection_items, only: [ :index, :create, :destroy ]
   resources :wishlist_items, only: [ :index, :create, :destroy ]
   get "search", to: "search#index"
