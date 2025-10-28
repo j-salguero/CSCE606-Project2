03d0f7c5fabda2114cd67899f36c645dad9741cf
Amira8399 <amiraw_2007@tamu.edu>
2025-10-27 19:18:46 -0500

    Fix trailing whitespace lint error

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get "wishlist_items/index"
   get "collection_items/index"
   get "collection", to: "albums#collection", as: :collection
-  
+
   root "albums#index"
   resources :collection_items, only: [ :index, :create, :destroy ]
   resources :wishlist_items, only: [ :index, :create, :destroy ]
