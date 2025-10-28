507f91e278771f38b0b413d4065b16c62d7f029f
Amira8399 <amiraw_2007@tamu.edu>
2025-10-27 20:01:07 -0500

    Fix: RuboCop style issues

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,13 +1,13 @@
-require 'ostruct'
+require "ostruct"
 class HomeController < ApplicationController
   def index
     @collection_items = CollectionItem.all
 
     if @collection_items.empty?
       @collection_items = [
-        OpenStruct.new(title: "Thriller", artist: "Michael Jackson" , year:1982),
-        OpenStruct.new(title: "Back to Black", artist: "Amy Winehouse", year:2006),
-        OpenStruct.new(title: "Random Access Memories", artist: "Daft Punk", year:2013)
+        OpenStruct.new(title: "Thriller", artist: "Michael Jackson", year: 1982),
+        OpenStruct.new(title: "Back to Black", artist: "Amy Winehouse", year: 2006),
+        OpenStruct.new(title: "Random Access Memories", artist: "Daft Punk", year: 2013)
       ]
     end
   end
