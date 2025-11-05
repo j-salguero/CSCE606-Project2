2c3b2488d0576d31999c7c0f645bfb208e8107f9
Jonathon Vega <jfvega@live.com>
2025-11-04 22:30:47 -0600

    updated session controller spec tests

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,7 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe SessionsController, type: :controller do
-    describe 'when trying to log in to the main application' do
-        it ''
+    let(:user) { User.create!(name: "Bob", email: "bob@email.com", password: "password") }
+    
+    describe 'when creating a new session' do
+        it 'log in user and redirect to collection_items page' do
+            post :create, params: { email: user.email, password: "password" }
+            expect(session[:user_id]).to eq(user.id)
+            expect(response).to redirect_to(collection_items_path)
+            expect(flash[:notice]).to eq("Welcome back to VinylVerse, #{user.name}!")
+        end
+
+        it 'invalid credentials does not log in' do
+            post :create, params: { email: user.email, password: "wrongpassword" }
+            expect(session[:user_id]).to be_nil
+            expect(response).to render_template(:new)
+            expect(response).to have_http_status(:unprocessable_entity)
+            expect(flash.now[:alert]).to eq("Invalid email or password")
+        end
+    end
+
+    describe 'when logging out of session' do
+        it 'should log out user and redirects to login page' do
+            delete :destroy
+            expect(session[:user_id]).to be_nil
+            expect(response).to redirect_to(login_path)
+            expect(flash[:notice]).to eq("You have been logged out.")
+        end
     end
 end
